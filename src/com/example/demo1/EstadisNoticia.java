39c0fd5257dc5f2a6085da00d16764c912b6780a
Mictlan <mictlan.armand@gmail.com>
2014-09-24 22:06:48 -0600

    demo1

---
+++ src/com/example/demo1/EstadisNoticia.java
@@ -0,0 +1,15 @@
+package com.example.demo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class EstadisNoticia extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_estadis_noticia);
+	}
+}
